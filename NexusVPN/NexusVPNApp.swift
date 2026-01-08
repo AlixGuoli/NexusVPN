384ae41a67b271355d784c7f756155daddb6f37b
AlixGuoli <1425300103@qq.com>
2026-01-08 16:11:10 +0800

    Initial Commit

---
+++ NexusVPN/NexusVPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  NexusVPNApp.swift
+//  NexusVPN
+//
+//  Created by ersao on 2026/1/8.
+//
+
+import SwiftUI
+
+@main
+struct NexusVPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
